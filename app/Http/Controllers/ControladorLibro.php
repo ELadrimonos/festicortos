8ae570038ae6bbab7967f854b6380d61a28f8561
Adrian Primo <adrpriber@alu.edu.gva.es>
2024-01-19 16:08:38 +0100

    Conexion a BBDD Biblioteca

--- app/Http/Controllers/ControladorLibro.php
+++ app/Http/Controllers/ControladorLibro.php
@@ -3,20 +3,23 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Libro;
 class ControladorLibro extends Controller
 {
     public function index()
     {
-        $libros = array(
-            array("titulo" => "El juego de Ender",
-                "autor" => "Orson Scott Card"),
-            array("titulo" => "La tabla de Flandes",
-                "autor" => "Arturo Pérez Reverte"),
-            array("titulo" => "La historia interminable",
-                "autor" => "Michael Ende"),
-            array("titulo" => "El Señor de los Anillos",
-                "autor" => "J.R.R. Tolkien")
-        );
+//        $libros = array(
+//            array("titulo" => "El juego de Ender",
+//                "autor" => "Orson Scott Card"),
+//            array("titulo" => "La tabla de Flandes",
+//                "autor" => "Arturo Pérez Reverte"),
+//            array("titulo" => "La historia interminable",
+//                "autor" => "Michael Ende"),
+//            array("titulo" => "El Señor de los Anillos",
+//                "autor" => "J.R.R. Tolkien")
+//        );
+        $libros = Libro::get();
+        //$autores = Autor::get();
         return view('libros', compact('libros'));
 
     }
