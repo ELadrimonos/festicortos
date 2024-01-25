41bf20212a0071de14a7d83f1fc4a5d40cb72279
Adrian Primo <adrpriber@alu.edu.gva.es>
2024-01-25 17:42:50 +0100

    Modificar funciona

--- app/Http/Controllers/ControladorLibro.php
+++ app/Http/Controllers/ControladorLibro.php
@@ -43,22 +43,18 @@ class ControladorLibro extends Controller
     public function edit(string $id)
     {
         $libro = Libro::findOrFail($id);
-        return view('libros_form', compact('libro'));
+        return view('libros_form', compact('libro', 'id'));
     }
 
     public function update(Request $request, string $id)
     {
-        // Validate the request
-        $request->validate([
-            'title' => 'required|max:255',
-            'author' => 'required|max:255',
-            // Add other validation rules as needed
-        ]);
 
         // Find the Libro instance by ID and update its attributes
         $libro = Libro::findOrFail($id);
-        $libro->title = $request->input('title');
-        $libro->author = $request->input('author');
+        $libro->titulo = $request->input('titulo');
+        $libro->editorial = $request->input('editorial');
+        $libro->precio = $request->input('precio');
+        $libro->id_autor = $request->input('id_autor');
         // Update other attributes as needed
         $libro->save();
 
